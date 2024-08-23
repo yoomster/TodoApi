3e1bae912987f50cf8a2048be6851e78d2ac12c2
yoomster <nperenboom@hotmail.com>
2024-08-23 14:23:39 +0200

    latest

---
+++ TodoApi/TodoDb.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using TodoApi;
+
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
